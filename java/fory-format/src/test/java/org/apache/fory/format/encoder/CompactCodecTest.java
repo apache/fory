2070bec6922165830b5c1688bdf105776ba6177a
Steven Schlansker <stevenschlansker@gmail.com>
2025-07-11 15:45:24 -0700

    wip

--- java/fory-format/src/test/java/org/apache/fory/format/encoder/CompactCodecTest.java
+++ java/fory-format/src/test/java/org/apache/fory/format/encoder/CompactCodecTest.java
@@ -250,4 +250,31 @@ public class CompactCodecTest {
     assertEquals(deserializedBean, bean1);
     assertEquals(buffer.size(), 7 + 12 + 8 + 16 * 3);
   }
+
+  @Data
+  public static class InlinePrimitiveNestedArrayType {
+    public InlineNestedType f1;
+    public String f2;
+    public short[] f3;
+  }
+
+  @Test
+  public void testInlinePrimitiveNestedArrayType() {
+    final InlinePrimitiveNestedArrayType bean1 = new InlinePrimitiveNestedArrayType();
+    bean1.f1 = new InlineNestedType();
+    bean1.f1.f1 = new Nested1();
+    bean1.f1.f1.f1 = 42;
+    bean1.f1.f2 = new Nested2();
+    bean1.f1.f2.f1 = 75;
+    bean1.f2 = "luna";
+    bean1.f3 = new short[] {1, 2, 3};
+    final RowEncoder<InlinePrimitiveNestedArrayType> encoder =
+        Encoders.buildBeanCodec(InlinePrimitiveNestedArrayType.class).compactEncoding().build().get();
+    final BinaryRow row = encoder.toRow(bean1);
+    final MemoryBuffer buffer = MemoryUtils.wrap(row.toBytes());
+    row.pointTo(buffer, 0, buffer.size());
+    final InlinePrimitiveNestedArrayType deserializedBean = encoder.fromRow(row);
+    assertEquals(deserializedBean, bean1);
+    assertEquals(buffer.size(), 7 + 12 + 8 + 2 * 3);
+  }
 }
