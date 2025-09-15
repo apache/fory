f09bdd5c896fda43c46247d93370634d9975af5a
JarVis <2055543978@qq.com>
2025-09-15 09:37:20 +0800

    restore struct.go

--- go/fory/struct.go
+++ go/fory/struct.go
@@ -141,6 +141,21 @@ func createStructFieldInfos(f *Fory, type_ reflect.Type) (structFieldsInfo, erro
 			field.Type = reflect.ValueOf(field.Type).Elem().Type()
 		}
 		fieldSerializer, _ := f.typeResolver.getSerializerByType(field.Type, true)
+		if field.Type.Kind() == reflect.Array {
+			// When a struct field is an array type,
+			// retrieve its corresponding slice serializer and populate it into fieldInfo for reuse.
+			elemType := field.Type.Elem()
+			sliceType := reflect.SliceOf(elemType)
+			fieldSerializer = f.typeResolver.typeToSerializers[sliceType]
+		} else if field.Type.Kind() == reflect.Slice {
+			// If the field is a concrete slice type, dynamically create a valid serializer
+			// so it has the potential and capability to use readSameTypes function.
+			if field.Type.Elem().Kind() != reflect.Interface {
+				fieldSerializer = sliceSerializer{
+					f.typeResolver.typesInfo[field.Type.Elem()],
+				}
+			}
+		}
 		f := fieldInfo{
 			name:         SnakeCase(field.Name), // TODO field name to lower case
 			field:        field,
