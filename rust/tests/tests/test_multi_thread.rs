13cfb1fd3bd4e0027ceaf144674d155887a12aff
urlyy <liuyuyang0201@gmail.com>
2025-10-10 06:36:57 +0800

    add a multi-thread ut

--- rust/tests/tests/test_multi_thread.rs
+++ rust/tests/tests/test_multi_thread.rs
@@ -16,12 +16,13 @@
 // under the License.
 
 use fory_core::Fory;
+use fory_derive::ForyObject;
 use std::collections::HashSet;
 use std::sync::Arc;
 use std::thread;
 
 #[test]
-fn test_multi_thread() {
+fn test_simple_multi_thread() {
     let fory = Arc::new(Fory::default());
     let src: HashSet<_> = [41, 42, 43, 45, 46, 47].into_iter().collect();
     // serialize
@@ -43,6 +44,53 @@ fn test_multi_thread() {
     for bytes in serialized_data {
         let fory_clone = Arc::clone(&fory);
         let handle = thread::spawn(move || fory_clone.deserialize::<i32>(&bytes).unwrap());
+        handles.push(handle);
+    }
+    for handle in handles {
+        let value = handle.join().unwrap();
+        dest.insert(value);
+    }
+    // verify
+    assert_eq!(dest, src);
+}
+
+#[test]
+fn test_struct_multi_thread() {
+    #[derive(ForyObject, Debug, PartialEq, Eq, Hash, Clone, Copy)]
+    struct Item1 {
+        f1: i32,
+    }
+    let mut fory = Fory::default();
+    fory.register::<Item1>(101);
+    let fory = Arc::new(fory);
+    let src: HashSet<_> = [
+        Item1 { f1: 42 },
+        Item1 { f1: 43 },
+        Item1 { f1: 45 },
+        Item1 { f1: 46 },
+        Item1 { f1: 47 },
+    ]
+    .into_iter()
+    .collect();
+    // serialize
+    let mut handles = vec![];
+    for item in &src {
+        let fory_clone = Arc::clone(&fory);
+        let item = *item;
+        let handle = thread::spawn(move || fory_clone.serialize(&item));
+        handles.push(handle);
+    }
+    let mut serialized_data = vec![];
+    for handle in handles {
+        let bytes = handle.join().unwrap();
+        serialized_data.push(bytes);
+    }
+    // deserialize
+    let mut dest = HashSet::new();
+    let mut handles = vec![];
+    for bytes in serialized_data {
+        let fory_clone = Arc::clone(&fory);
+        let handle = thread::spawn(move || fory_clone.deserialize::<Item1>(&bytes).unwrap());
         handles.push(handle);
     }
     for handle in handles {
