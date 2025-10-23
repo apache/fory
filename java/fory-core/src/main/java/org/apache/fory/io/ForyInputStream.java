c2135d51ff9353e6febc5058d9aacea03eb0677d
muzhiyuan10607 <739061411@qq.com>
2025-10-23 16:36:23 +0800

    bugfix-2772：修改读取EOF流异常不一致问题

--- java/fory-core/src/main/java/org/apache/fory/io/ForyInputStream.java
+++ java/fory-core/src/main/java/org/apache/fory/io/ForyInputStream.java
@@ -127,14 +127,14 @@ public class ForyInputStream extends InputStream implements ForyStreamReader {
       len -= remaining;
       dstIndex += remaining;
       try {
-        int read = stream.read(dst, dstIndex, len);
-        while (read < len) {
+        int read = 0;
+        do {
           int newRead = stream.read(dst, dstIndex + read, len - read);
           if (newRead < 0) {
             throw new IndexOutOfBoundsException("No enough data in the stream " + stream);
           }
           read += newRead;
-        }
+        } while (read < len);
       } catch (IOException e) {
         throw new RuntimeException(e);
       }
