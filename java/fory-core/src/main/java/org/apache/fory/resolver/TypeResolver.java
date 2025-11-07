42bf59cdda3562a1f18208d879864052494f5693
Mikhail Chernyakov <mychernyakov@gmail.com>
2025-11-07 15:02:19 +0100

    checkstyle fix

--- java/fory-core/src/main/java/org/apache/fory/resolver/TypeResolver.java
+++ java/fory-core/src/main/java/org/apache/fory/resolver/TypeResolver.java
@@ -474,9 +474,9 @@ public abstract class TypeResolver {
 
   public abstract Class<? extends Serializer> getSerializerClass(Class<?> cls);
 
-  public abstract Class<? extends Serializer> getSerializerClassFinalField(Class<?> cls);
-
   public abstract Class<? extends Serializer> getSerializerClass(Class<?> cls, boolean codegen);
+
+  public abstract Class<? extends Serializer> getSerializerClassFinalField(Class<?> cls);
 
   public final boolean isCollection(Class<?> cls) {
     if (Collection.class.isAssignableFrom(cls)) {
