aa2dee55cdfe812d57950d736a8a7f18a2dfcf63
chaokunyang <shawn.ck.yang@gmail.com>
2026-02-19 02:32:22 +0800

    add tests case to subclass

--- java/fory-core/src/test/java/org/apache/fory/xlang/SwiftXlangTest.java
+++ java/fory-core/src/test/java/org/apache/fory/xlang/SwiftXlangTest.java
@@ -86,4 +86,222 @@ public class SwiftXlangTest extends XlangTestBase {
     env.put("ENABLE_FORY_DEBUG_OUTPUT", "1");
     return new CommandContext(command, env, new File("../../swift"));
   }
+
+  // ============================================================================
+  // Test methods - duplicated from XlangTestBase for Maven Surefire discovery
+  // ============================================================================
+
+  @Test
+  public void testBuffer() throws java.io.IOException {
+    super.testBuffer();
+  }
+
+  @Test
+  public void testBufferVar() throws java.io.IOException {
+    super.testBufferVar();
+  }
+
+  @Test
+  public void testMurmurHash3() throws java.io.IOException {
+    super.testMurmurHash3();
+  }
+
+  @Test
+  public void testStringSerializer() throws Exception {
+    super.testStringSerializer();
+  }
+
+  @Test
+  public void testCrossLanguageSerializer() throws Exception {
+    super.testCrossLanguageSerializer();
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testSimpleStruct(boolean enableCodegen) throws java.io.IOException {
+    super.testSimpleStruct(enableCodegen);
+  }
+
+  @Test
+  public void testSimpleNamedStructCodegenEnabled() throws java.io.IOException {
+    super.testSimpleNamedStruct(false);
+  }
+
+  @Test
+  public void testSimpleNamedStructCodegenDisabled() throws java.io.IOException {
+    super.testSimpleNamedStruct(false);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testList(boolean enableCodegen) throws java.io.IOException {
+    super.testList(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testMap(boolean enableCodegen) throws java.io.IOException {
+    super.testMap(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testInteger(boolean enableCodegen) throws java.io.IOException {
+    super.testInteger(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testItem(boolean enableCodegen) throws java.io.IOException {
+    super.testItem(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testColor(boolean enableCodegen) throws java.io.IOException {
+    super.testColor(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testStructWithList(boolean enableCodegen) throws java.io.IOException {
+    super.testStructWithList(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testStructWithMap(boolean enableCodegen) throws java.io.IOException {
+    super.testStructWithMap(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testCollectionElementRefOverride(boolean enableCodegen) throws java.io.IOException {
+    super.testCollectionElementRefOverride(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testSkipIdCustom(boolean enableCodegen) throws java.io.IOException {
+    super.testSkipIdCustom(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testSkipNameCustom(boolean enableCodegen) throws java.io.IOException {
+    super.testSkipNameCustom(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testConsistentNamed(boolean enableCodegen) throws java.io.IOException {
+    super.testConsistentNamed(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testStructVersionCheck(boolean enableCodegen) throws java.io.IOException {
+    super.testStructVersionCheck(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testPolymorphicList(boolean enableCodegen) throws java.io.IOException {
+    super.testPolymorphicList(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testPolymorphicMap(boolean enableCodegen) throws java.io.IOException {
+    super.testPolymorphicMap(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testOneStringFieldSchemaConsistent(boolean enableCodegen) throws java.io.IOException {
+    super.testOneStringFieldSchemaConsistent(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testOneStringFieldCompatible(boolean enableCodegen) throws java.io.IOException {
+    super.testOneStringFieldCompatible(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testTwoStringFieldCompatible(boolean enableCodegen) throws java.io.IOException {
+    super.testTwoStringFieldCompatible(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testSchemaEvolutionCompatible(boolean enableCodegen) throws java.io.IOException {
+    super.testSchemaEvolutionCompatible(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testOneEnumFieldSchemaConsistent(boolean enableCodegen) throws java.io.IOException {
+    super.testOneEnumFieldSchemaConsistent(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testOneEnumFieldCompatible(boolean enableCodegen) throws java.io.IOException {
+    super.testOneEnumFieldCompatible(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testTwoEnumFieldCompatible(boolean enableCodegen) throws java.io.IOException {
+    super.testTwoEnumFieldCompatible(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testEnumSchemaEvolutionCompatible(boolean enableCodegen) throws java.io.IOException {
+    super.testEnumSchemaEvolutionCompatible(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testNullableFieldSchemaConsistentNotNull(boolean enableCodegen)
+      throws java.io.IOException {
+    super.testNullableFieldSchemaConsistentNotNull(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testNullableFieldSchemaConsistentNull(boolean enableCodegen)
+      throws java.io.IOException {
+    super.testNullableFieldSchemaConsistentNull(enableCodegen);
+  }
+
+  @Override
+  @Test(dataProvider = "enableCodegen")
+  public void testNullableFieldCompatibleNotNull(boolean enableCodegen) throws java.io.IOException {
+    super.testNullableFieldCompatibleNotNull(enableCodegen);
+  }
+
+  @Override
+  @Test(dataProvider = "enableCodegen")
+  public void testNullableFieldCompatibleNull(boolean enableCodegen) throws java.io.IOException {
+    super.testNullableFieldCompatibleNull(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testUnionXlang(boolean enableCodegen) throws java.io.IOException {
+    super.testUnionXlang(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testRefSchemaConsistent(boolean enableCodegen) throws java.io.IOException {
+    super.testRefSchemaConsistent(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testRefCompatible(boolean enableCodegen) throws java.io.IOException {
+    super.testRefCompatible(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testCircularRefSchemaConsistent(boolean enableCodegen) throws java.io.IOException {
+    super.testCircularRefSchemaConsistent(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testCircularRefCompatible(boolean enableCodegen) throws java.io.IOException {
+    super.testCircularRefCompatible(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testUnsignedSchemaConsistent(boolean enableCodegen) throws java.io.IOException {
+    super.testUnsignedSchemaConsistent(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testUnsignedSchemaConsistentSimple(boolean enableCodegen) throws java.io.IOException {
+    super.testUnsignedSchemaConsistentSimple(enableCodegen);
+  }
+
+  @Test(dataProvider = "enableCodegen")
+  public void testUnsignedSchemaCompatible(boolean enableCodegen) throws java.io.IOException {
+    super.testUnsignedSchemaCompatible(enableCodegen);
+  }
 }
