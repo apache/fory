08e1a8976670ebbb0b3ef94e4d2919949ca07e84
Tyooughtul <yooughtul@126.com>
2026-02-27 09:37:31 +0800

    fix: remove head liscese

--- ci/extract_cpp_doc_code.py
+++ ci/extract_cpp_doc_code.py
@@ -159,22 +159,6 @@ def wrap_code_as_test(code: str, doc_file: str, block_index: int) -> str:
         if "using namespace" not in code:
             code = f"using namespace fory::serialization;\n\n{code}"
         return f"""// Auto-generated test from {doc_file}
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
 
 {include_section}
 
@@ -182,22 +166,6 @@ def wrap_code_as_test(code: str, doc_file: str, block_index: int) -> str:
 """
     else:
         return f"""// Auto-generated test from {doc_file}, block {block_index}
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
 
 #include <iostream>
 {include_section}
@@ -252,27 +220,7 @@ def process_markdown_file(md_path: Path, output_dir: Path) -> List[Path]:
 def generate_bazel_build(test_files: List[Path], output_dir: Path) -> None:
     build_path = output_dir / "BUILD"
 
-    build_content = """# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
-# Auto-generated BUILD file for documentation example tests
-# Run: bazel test //:doc_example_tests
-
-package(default_visibility = ["//visibility:public"])
+    build_content = """package(default_visibility = ["//visibility:public"])
 
 """
 
