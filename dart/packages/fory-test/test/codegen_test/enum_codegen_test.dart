1c56dfe3f74257923d54e4cac4eabcc6130edb65
yash <yash162534@gmail.com>
2026-03-15 01:21:08 +0530

    add tests

--- dart/packages/fory-test/test/codegen_test/enum_codegen_test.dart
+++ dart/packages/fory-test/test/codegen_test/enum_codegen_test.dart
@@ -22,6 +22,7 @@ library;
 
 import 'package:checks/checks.dart';
 import 'package:fory/fory.dart';
+import 'package:fory_test/entity/enum_id_foo.dart';
 import 'package:fory_test/entity/enum_foo.dart';
 import 'package:test/test.dart';
 
@@ -31,8 +32,24 @@ void main() {
       EnumSpec enumSpec = EnumSpec(EnumFoo, [EnumFoo.A, EnumFoo.B]);
       EnumSpec enumSubTypeSpec =
           EnumSpec(EnumSubClass, [EnumSubClass.A, EnumSubClass.B]);
+      EnumSpec enumWithIdsSpec = EnumSpec(
+        EnumWithIds,
+        [EnumWithIds.A, EnumWithIds.B, EnumWithIds.C],
+        {
+          10: EnumWithIds.A,
+          20: EnumWithIds.B,
+          30: EnumWithIds.C,
+        });
+      EnumSpec enumPartialIdsSpec =
+          EnumSpec(EnumPartialIds,[EnumPartialIds.A, EnumPartialIds.B, EnumPartialIds.C]);
+      EnumSpec enumDuplicateIdsSpec = 
+          EnumSpec(EnumDuplicateIds, [EnumDuplicateIds.A, EnumDuplicateIds.B, EnumDuplicateIds.C]);
+
       check($EnumFoo).equals(enumSpec);
       check($EnumSubClass).equals(enumSubTypeSpec);
+      check($EnumWithIds).equals(enumWithIdsSpec);
+      check($EnumPartialIds).equals(enumPartialIdsSpec);
+      check($EnumDuplicateIds).equals(enumDuplicateIdsSpec);
     });
   });
 }
